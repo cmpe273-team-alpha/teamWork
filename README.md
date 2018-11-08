883a92cc29fdb452023f419b135c23c268fc5371
Huy Vo <huyvanvo94@gmail.com>
2018-11-08 03:12:51 -0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # CMPE 273 Demo 
+
+#### Notes 
+```
+NewSQL is a class of modern relational database management systems that seek to provide the same scalable performance of NoSQL systems for 
+online transaction processing (OLTP) read-write workloads while still maintaining the ACID guarantees of a traditional database system. 
+
+The lack of transactions in Bigtable led to frequent complaints from users, so Google made distributed transactions central to Spanner's design. 
+
+
+
+Based on its experience with Bigtable, Google argues that it is better to have application programmers deal with 
+performance problems due to overuse of transactions as bottlenecks arise, rather than always coding around the lack of transactions. 
+
+Spanner uses the Paxos algorithm as part of its operation to shard data across hundreds of datacenters.
+It makes heavy use of hardware-assisted clock synchronization using GPS clocks and atomic clocks to ensure global consistency.
+
+About Spanner: 
+
+Schema
+SQL
+Consistency 
+Availability
+Scalability
+Replication 
+```
--- spanner_demo/snippets.py
+++ spanner_demo/snippets.py
@@ -85,10 +85,150 @@ def query_data(instance_id, database_id):
             print(u'SingerId: {}, AlbumId: {}, AlbumTitle: {}'.format(*row))
 # [END spanner_query_data]
 
+def read_data(instance_id, database_id):
+    """Reads sample data from the database."""
+    spanner_client = spanner.Client()
+    instance = spanner_client.instance(instance_id)
+    database = instance.database(database_id)
+
+    with database.snapshot() as snapshot:
+        keyset = spanner.KeySet(all_=True)
+        results = snapshot.read(
+            table='Albums',
+            columns=('SingerId', 'AlbumId', 'AlbumTitle',),
+            keyset=keyset,)
+
+        for row in results:
+            print(u'SingerId: {}, AlbumId: {}, AlbumTitle: {}'.format(*row))
+
+def read_write_transaction(instance_id, database_id):
+    """Performs a read-write transaction to update two sample records in the
+    database.
+
+    This will transfer 200,000 from the `MarketingBudget` field for the second
+    Album to the first Album. If the `MarketingBudget` is too low, it will
+    raise an exception.
+
+    Before running this sample, you will need to run the `update_data` sample
+    to populate the fields.
+    """
+    spanner_client = spanner.Client()
+    instance = spanner_client.instance(instance_id)
+    database = instance.database(database_id)
+
+    def update_albums(transaction):
+        # Read the second album budget.
+        second_album_keyset = spanner.KeySet(keys=[(2, 2)])
+        second_album_result = transaction.read(
+            table='Albums', columns=('MarketingBudget',),
+            keyset=second_album_keyset, limit=1)
+        second_album_row = list(second_album_result)[0]
+        second_album_budget = second_album_row[0]
+
+        transfer_amount = 200000
+
+        if second_album_budget < 300000:
+            # Raising an exception will automatically roll back the
+            # transaction.
+            raise ValueError(
+                'The second album doesn\'t have enough funds to transfer')
+
+        # Read the first album's budget.
+        first_album_keyset = spanner.KeySet(keys=[(1, 1)])
+        first_album_result = transaction.read(
+            table='Albums', columns=('MarketingBudget',),
+            keyset=first_album_keyset, limit=1)
+        first_album_row = list(first_album_result)[0]
+        first_album_budget = first_album_row[0]
+
+        # Update the budgets.
+        second_album_budget -= transfer_amount
+        first_album_budget += transfer_amount
+        print(
+            'Setting first album\'s budget to {} and the second album\'s '
+            'budget to {}.'.format(
+                first_album_budget, second_album_budget))
+
+        # Update the rows.
+        transaction.update(
+            table='Albums',
+            columns=(
+                'SingerId', 'AlbumId', 'MarketingBudget'),
+            values=[
+                (1, 1, first_album_budget),
+                (2, 2, second_album_budget)])
+
+    database.run_in_transaction(update_albums)
+
+    print('Transaction complete.')
+
+def add_column(instance_id, database_id):
+    """Adds a new column to the Albums table in the example database."""
+    spanner_client = spanner.Client()
+    instance = spanner_client.instance(instance_id)
+    database = instance.database(database_id)
+
+    operation = database.update_ddl([
+        'ALTER TABLE Albums ADD COLUMN MarketingBudget INT64'])
+
+    print('Waiting for operation to complete...')
+    operation.result()
+
+    print('Added the MarketingBudget column.')
+
+def update_data(instance_id, database_id):
+    """Updates sample data in the database.
+
+    This updates the `MarketingBudget` column which must be created before
+    running this sample. You can add the column by running the `add_column`
+    sample or by running this DDL statement against your database:
+
+        ALTER TABLE Albums ADD COLUMN MarketingBudget INT64
+
+    """
+    spanner_client = spanner.Client()
+    instance = spanner_client.instance(instance_id)
+    database = instance.database(database_id)
+
+    with database.batch() as batch:
+        batch.update(
+            table='Albums',
+            columns=(
+                'SingerId', 'AlbumId', 'MarketingBudget'),
+            values=[
+                (1, 1, 100000),
+                (2, 2, 500000)])
+
+    print('Updated data.')
+
 # python3.7 snippets.py test-instance --database-id example-db create_database
+
+
+"""
+Create a database 
+"""
+
+# 1.
 # create_database( INSTANCE_ID , DATABASE_ID )
 
+# 2.
 # insert_data(INSTANCE_ID, DATABASE_ID)
 
-query_data(INSTANCE_ID, DATABASE_ID)
+# 3.
+# query_data(INSTANCE_ID, DATABASE_ID)
 
+# 4.
+
+# read_data(INSTANCE_ID, DATABASE_ID)
+
+# 5.
+
+# add_column(INSTANCE_ID, DATABASE_ID)
+
+# 6.
+
+# update_data(INSTANCE_ID, DATABASE_ID)
+
+# 7.
+
+# read_write_transaction(INSTANCE_ID, DATABASE_ID)
